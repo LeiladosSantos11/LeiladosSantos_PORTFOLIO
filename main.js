7f5264ee3d20da7d1a822c634487b7b3c612b847
Leila dos Santos <28732563+LeiladosSantos11@users.noreply.github.com>
2018-07-15 18:24:29 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+
+function createParagraph2() {
+  console.log( "HTML" );
+}
+ar icons2 = document.querySelectorAll( 'span' );
+icons2.forEach( icon2 => icon2.addEventListener( 'click', createParagraph2 ) );
+
+
+
+
+
+  /*
+function createParagraph2() {
+  console.log( "HTML" );
+}
+
+var icons2 = document.querySelectorAll( 'span' );
+icons2.forEach( icon2 => icon2.addEventListener( 'click', createParagraph2 ) );
+
+
+
+  <i class="icon-html5-alt"></i> 
+					<i class="icon-css3-alt"></i>
+					<i class="icon-javascript-alt" id="iconJS"></i>
+					<i class="icon-jquery"></i>
+					<span style="text-decoration:underline;"><i class="icon-reactjs"></i></span>
+					<span style="text-decoration:underline;"><i class="icon-nodejs"></i></span>
+                    <i class="fab fa-github-square" id="icon"></i>
+
+                    
+  var icons = click.document.getElementsByClassName("icon-css3-alt");
+  icons.click.innerHTML = "HTLM";
+
+
+  
+var icons = document.querySelectorAll("span")
+
+
+}
+  for (var i = 0; > i.length i ++); {
+icons[i].addEventListener('click', createParagraph);
+                    */
